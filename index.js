88f27954761ca7933d415c1d89048bcbcffcc9b2
Mathias Bretonneau <Mathias.bretonneau@gmail.com>
2022-05-03 13:26:28 +0200

    Add some fix on the js script

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 function fetchSimpsonJSON() {
-    
-    const simpsonId = 1;
-    const url = `https://simpsons-quotes-api.herokuapp.com/quotes${simpsonId}`;
-    axios.get(url)
-      .then(function(response) {
-        return response.data; 
-      })
-      .then(function(simpson) {
-        console.log('data decoded from JSON:', simpson);
-  
-        const simpsonHtml = `
-          <p><strong>${simpson.quote}</strong></p>
-          <p><strong>${simpson.character}</strong></p>
-          <img src="${simpson.image}" />
-          <p><strong>${simpson.characterDirection}</strong></p>
-        `;
-        document.querySelector('#simpson').innerHTML = simpsonHtml;
-      });
-  }
+  const simpson = 1;
+  const url = `https://simpsons-quotes-api.herokuapp.com/quotes ${simpsonId}`;
+  axios.get(url)
+    .then(function(simpson) {
+      return simpson.data; 
+    })
+    .then(function(simpson) {
+      console.log('data decoded from JSON:', simpson);
+      const simpsonHtml = `
+        <p><strong>${simpson[0].character}</strong></p>
+        <img src="${simpson[0].image }" />
+        <p><strong>${simpson[0].quote}</strong></p>`;
+      document.querySelector('#simpson').innerHTML = simpsonHtml;
+    });
+}
 
-let elementButton = document.getElementById('load');
-elementButton.addEventListener('click', function () {
-fetchSimpsonJSON();
-
-});
-
+fetchsimpsonJSON();
   
